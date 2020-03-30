e1b0898bb580e69886911f432a5b658ebda6750a
Soltane <soltanne92@gmail.com>
2020-03-30 14:18:22 -0500

    secure private info with ENV file

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Soltane:Soltane92@react-blog-xjsli.mongodb.net/test?retryWrites=true&w=majority',
-{useNewUrlParser: true}).then(() => console.log('DB connected')).catch(err => console.log.error(error));
-app.get('/', (req,res) => {
-    res.send('hello world')
-});
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const db = require('./config/key').mongoURI
+
+const { User } = require('./models/user');
+
+
+//* connected to mongoDB
+mongoose.connect(db, {useNewUrlParser: true}).then(() => console.log('DB connected')).catch(err => console.log.error(error));
+
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//* Welcome Page
+app.post('/api/user/register', (req,res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json ({ succes: false, err })
+        return res.status(200).json({ success:true})
+    })
+    });
 
 
 
