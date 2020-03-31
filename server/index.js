a55ec41f94bfc5c04901cc4825b864e75d111a16
Soltane <soltanne92@gmail.com>
2020-03-31 13:04:57 -0500

    New update v2

--- server/index.js
+++ server/index.js
@@ -82,7 +82,8 @@ app.get("/api/user/logout", auth, (req, res) => {
     });
 });
 
-const port = process.env.Port || 5000
+const port = process.env.PORT || 5000
+
 app.listen(port, () => {
     console.log(`server started on port ${port}`)
 });
