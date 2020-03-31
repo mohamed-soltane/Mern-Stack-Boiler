946fa4ed36fac7c0c5e45f286f724194bdcfa1c7
Soltane <soltanne92@gmail.com>
2020-03-31 12:59:43 -0500

    New update v1

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/api/user/auth', (req,res) => {
+app.get('/',  (req,res) => {
+    res.json({ "hello" : "my application mf"})
+});
+
+
+app.get('/api/user/auth', auth,  (req,res) => {
    res.status(200).json({
    _id:req._id,
    isAuth: true,
